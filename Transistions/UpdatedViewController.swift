c9870d052b8c6dd785e8310bb4e46dcfc8177ea6
Kadin Hatchell <kadinahatchell@gmail.com>
2016-10-03 07:57:17 -0600

    Linked the adde ViewController to the appropriate .swift

---
+++ Transistions/UpdatedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpdatedViewController.swift
+//  Transistions
+//
+//  Created by Hatchell, Kadin on 10/3/16.
+//  Copyright © 2016 Hatchell, Kadin. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Load this pages components.
+    }
+    
+}
