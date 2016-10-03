86993701108d79f1f44ff5c39ca908dd5f017c2e
Kadin Hatchell <kadinahatchell@gmail.com>
2016-10-03 07:46:03 -0600

    Initial Commit

---
+++ Transistions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Transistions
+//
+//  Created by Hatchell, Kadin on 10/3/16.
+//  Copyright © 2016 Hatchell, Kadin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
